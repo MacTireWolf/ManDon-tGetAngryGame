df837d0f8fabebbd762dc774c4aac2d3cd16b31d
Someone <martynaogrodniczak@gmail.com>
2024-11-23 17:24:03 +0100

    Finished Center

--- mandontgetangrygame/src/organisms/Center.js
+++ mandontgetangrygame/src/organisms/Center.js
@@ -6,7 +6,10 @@ const Center = () => {
   
   return (
     <div className="Center">
-      <Triangle color="blue" style={{ top: 0, left: 0 }} />
+      <Triangle colour="red" style={{ bottom: 0, right: 0 }} />
+      <Triangle colour="blue" style={{ top: 0, right: 0 }} />
+      <Triangle colour="green" style={{ bottom: 0, left: 0 }} />
+      <Triangle colour="yellow" style={{ top: 0, left: 0 }} />
     </div>
   );  
 };
