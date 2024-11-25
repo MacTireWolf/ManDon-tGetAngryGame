5d71f272f2d764a9803c160bfe345e4c6543e71c
Someone <martynaogrodniczak@gmail.com>
2024-11-25 16:43:35 +0100

    Added player's names and adjusted styling

--- mandontgetangrygame/src/components/Board.js
+++ mandontgetangrygame/src/components/Board.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components/Board.css";
 import Square from "../organisms/Square";
 import Center from "../organisms/Center";
@@ -8,13 +8,20 @@ import BlueVerticalCells from "../atoms/BlueVerticalCells";
 import GreenVerticalCells from "../atoms/GreenVerticalCells";
 
 const Board = () => {
+
+  const[playersNames, setPlayerNames] = useState({
+    red: "1",
+    blue: "2",
+    green: "3",
+    yellow: "4",
+  })
   
   return (
     <div className="Board">
-      <Square colour="red" style={{ top: 0, left: 0 }} />
-      <Square colour="blue" style={{ top: 0, right: 0 }} />
-      <Square colour="green" style={{ bottom: 0, left: 0 }} />
-      <Square colour="yellow" style={{ bottom: 0, right: 0 }} />
+      <Square colour="red" style={{ top: 0, left: 0 }} playerName={playersNames.red}/>
+      <Square colour="blue" style={{ top: 0, right: 0 }} playerName={playersNames.blue}/>
+      <Square colour="green" style={{ bottom: 0, left: 0 }} playerName={playersNames.green}/>
+      <Square colour="yellow" style={{ bottom: 0, right: 0 }} playerName={playersNames.yellow} />
       <Center/>
       <div className="horizontal-cells-container">
         <RedHorizontalCells />
--- mandontgetangrygame/src/organisms/Square.js
+++ mandontgetangrygame/src/organisms/Square.js
@@ -2,13 +2,16 @@ import React from "react";
 import "../styles/organisms/Square.css";
 import SquareWithinSquare from "../atoms/SquareWithinSquare";
 
-const Square = ({colour, style}) => {
-  
+const Square = ({ colour, style, playerName }) => {
+  const isTopPosition = colour === "red" || colour === "blue";
+
   return (
     <div className={`Square square-${colour}`} style={style}>
-      <SquareWithinSquare/>
+      <div className={`player-name ${isTopPosition ? "top" : "bottom"}`}>
+        {playerName}
+      </div>
+      <SquareWithinSquare />
     </div>
-    
   );
 };
 
